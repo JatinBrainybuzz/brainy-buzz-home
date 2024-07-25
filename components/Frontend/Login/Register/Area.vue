b517d9cd727c70bb2dec2672c8718784cc711256
JatinBrainyBuzz <jatin.kumar@brainybuzz.co>
2024-07-25 21:04:59 +0530

    Fix register page UI and added client side api call

--- components/Frontend/Login/Register/Area.vue
+++ components/Frontend/Login/Register/Area.vue
@@ -20,57 +20,55 @@
                     <span class="custom-message text-gray-500"> Welcome To Brainybuzz</span>
                 </div>
                 <div class="px-4 pt-4">
-                    <UForm :validate="validate" :state="state" class="space-y-4" @submit="onSubmit">
+                    <UForm :validate="validate" :state="state" class="space-y-4" @submit="submitForm" @error="onError">
                         <div class="relative mb-8">
                         
-                            <div class="h-12 bg-white relative mb-8">
-                                <!-- <input id="mobile" type="text" placeholder="Enter Your Username..."
+                            <div class="h-12 bg-white relative mb-9">
+                                <!-- <input id="mobile" type="text" placeholder="Enter Your Username..." v-model="state.name"
                                     class=" w-full h-full px-2 border border-gray-300 rounded-md text-base text-black" />
                                 <div class=" bg-white absolute -top-3 left-2 px-1 inline-block text-base text-black-title">
                                     <label for="Mobile">UserName</label>
                                 </div> -->
-                                <UFormGroup name="name" :required="false">
-                                    <UInput v-model="state.name"  placeholder="Enter Your Username..." />
-                                    <div class=" bg-white absolute -top-4 left-2 px-1 inline-block text-base">
-                                        <label for="Mobile">UserName</label>
-                                    </div>
+                                <UFormGroup label="UserName" name="name" required class="text-start">
+                                    <UInput icon="i-heroicons-user-circle-16-solid"
+                                    v-model="state.name"  placeholder="Enter Your Username..." />
                                 </UFormGroup>
                             </div>
 
-
-                            <div class="h-12 bg-white relative mb-7 ">
-                                <!-- <input id="mobile" type="email" placeholder="Enter Your Email"
+                            <div class="h-12 bg-white relative mb-10 ">
+                                <!-- <input id="mobile" type="email" placeholder="Enter Your Email" v-model="state.email" @input="validate"
                                     class="w-full h-full px-2 border border-gray-300 rounded-md text-base text-black" />
                                 <div class=" bg-white absolute -top-3 left-2 px-1 inline-block text-base text-black-title">
                                     <label for="Mobile">Email</label>
                                 </div> -->
-                                <UFormGroup name="email" :required="false">
-                                    <UInput v-model="state.email"  placeholder="Enter Your email..." />
-                                    <div class=" bg-white absolute -top-4 left-2 px-1 inline-block text-base">
-                                        <label for="Mobile">Email</label>
-                                    </div>
+                                <UFormGroup label="Email" name="email" required class="text-start">
+                                    <UInput v-model="state.email" icon="i-heroicons-envelope"
+                                    placeholder="Enter Your Email..." />
                                 </UFormGroup>
+                                
                             </div>
                             <div class="flex gap-2 items-center mb-7">
                                 <div class=" w-1/2 border-b border-gray-300">Date Of Birth</div>
                                 <!-- <input type="date" value="2024-05-13" class="w-1/2 border"> -->
                                 <div class=" w-full">
                                     <UPopover :popper="{ placement: 'bottom-start' }">
-                                        <UButton icon="i-heroicons-calendar-days-20-solid"
-                                            :label="format(date, 'd MMM, yyy')" block />
+                                        <UButton icon="i-heroicons-calendar-days-20-solid" :label="format(state.Newdate, 'd MMM, yyy')" block />
                                         <template #panel="{ close }">
-                                            <DatePicker v-model="date" is-required @close="close" />
+                                            <DatePicker v-model="state.Newdate" is-required @close="close" />
                                         </template>
+                                        <!-- :label="format(date, 'd MMM, yyy')"  -->
                                     </UPopover>
                                 </div>
-
                             </div>
-                            <div class="h-12 bg-white relative my-5">
-                                <input id="mobile" type="text" placeholder="Enter Your Mobile Number"
+                            <div class="h-10 bg-white relative my-5">
+                                <!-- <input id="mobile" type="text" placeholder="Enter Your Mobile Number" v-model="state.phone"
                                     class="w-full h-full px-2 border border-gray-300 rounded-md text-base text-black" />
                                 <div class=" bg-white absolute -top-3 left-2 px-1 inline-block text-base text-black-title">
                                     <label for="Mobile">Mobile Number</label>
-                                </div>
+                                </div> -->
+                                <UFormGroup label="Mobile Number" required name="mobile" class="text-start">
+                                    <UInput icon="i-heroicons-phone-arrow-down-left-solid" v-model="state.phone"  placeholder="Enter mobile number..."/>
+                                </UFormGroup>
                             </div>
                             <div class=" text-gray-600 text-left my-3">
                                 OTP will be sent on email in case of non-indian mobile number.
@@ -81,10 +79,13 @@
                                 <label for="notification" class=" cursor-pointer">Allow Notification</label>
                             </div>
 
-                            <!-- <div class="text-white bg-primary hover:shadow-lg my-2 py-2 rounded-md cursor-pointer">
+                            <!-- <div class="text-white bg-primary hover:shadow-lg my-2 py-2 rounded-md cursor-pointer" @click="submitForm">
                                 Sign Up
                             </div> -->
-                            <UButton  type="submit" color="primary" variant="solid" class="hover:shadow-lg my-2 py-2 rounded-md cursor-pointer">Sign up</UButton>
+                            <div class="text-center">
+                                <UButton  type="submit" color="primary" variant="solid" class="hover:shadow-lg w-full text-base justify-center rounded-md cursor-pointer">
+                                Sign up</UButton>
+                            </div>
                             <div class=" font-extralight flex gap-2 mt-4 justify-center">
                                 <span>Already Have an Account?</span>
                                 <div>
@@ -104,21 +105,66 @@
 
 <script setup lang="ts">
     import {
-        format
+        format,
+        isValid
     } from 'date-fns'
-    import type { FormError, FormSubmitEvent } from '#ui/types'
+    import type { FormError,FormErrorEvent, FormSubmitEvent } from '#ui/types'
     
     const date = ref(new Date())
     const state = reactive({
         email: undefined,
         name: undefined,
+        phone: undefined,
+        Newdate: new Date(),
     })
     const validate = (state: any): FormError[] => {
         const errors = []
-        if (!state.email) errors.push({ path: 'email', message: 'Required' })
+        if (!state.email) {
+            errors.push({ path: 'email', message: 'Required' })
+        }
         if (!state.name) errors.push({ path: 'name', message: 'Required' })
         return errors
     }
+    
+
+    async function onError (event: FormErrorEvent) {
+    const element = document.getElementById(event.errors[0].id)
+    element?.focus()
+    element?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+    }
+
+    function formatDate(date:Date): String {
+        return format(date, 'yyyy-MM-dd');
+    }
+    // const HOMEPAGE_API = ;
+    // async function submitform () {
+    //     const response = $fetch(config.public.appUrl+"/api/register",{
+    //         method: "POST",
+            
+    //     })
+    // }
+        const config = useRuntimeConfig();
+        const submitForm = async () => {
+            try{
+                const formattedDate = formatDate(state.Newdate);
+            const data:any = await $fetch(config.public.appUrl+"api/register", {
+                method: 'post',
+                body: {
+                    name: state.name,
+                    email: state.email,
+                    country: "91",
+                    date_of_birth: formattedDate,
+                    phone: state.phone
+                }
+            })
+            console.log('hello sir it worked')
+            console.log(data.value)
+        }
+        catch(error){
+            console.error('when submitting form got error: ', error)
+        }
+        }
+    // const {  data: items } = await useLazyFetch(HOMEPAGE_API);
 
     async function onSubmit (event: FormSubmitEvent<any>) {
     // Do something with data
